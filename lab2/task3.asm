c2500c2b069777c7c610ccab6a53823cc1d67929
Areeb Sajjad <89740222+asajjad2@users.noreply.github.com>
2021-09-29 08:46:00 +0500

    Create task3.asm

---
+++ lab2/task3.asm
@@ -0,0 +1,36 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+var1 DB ?
+var2 DB ?
+diff DB ?
+.CODE
+main proc
+MOV AX, @DATA
+MOV DS, AX
+mov ah,01
+
+int 21h
+mov var1,al
+sub var1,48
+mov ah,01
+int 21h
+mov var2,al
+sub var2,48
+mov al,var1
+mov ah,0000
+mov bl,var2
+mul bl
+add ax,48
+mov dx,ax
+mov ah,02
+int 21h
+mov dl,dh
+mov ah,02
+int 21h
+MOV AH,4CH
+INT 21H
+main endp
+end main
+main endp
+end main
